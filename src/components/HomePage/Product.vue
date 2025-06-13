52394f1d85eeda683a2212d3d7442714789286c0
supawit wongarran <spwpeem@gmail.com>
2025-06-13 15:31:35 +0700

    Refactor Product and ProductDetail components for improved UI and functionality

--- src/components/HomePage/Product.vue
+++ src/components/HomePage/Product.vue
@@ -3,23 +3,35 @@
     <v-row>
       <v-col cols="12">
         <!-- Input ค้นหา -->
-    <v-text-field
-      v-model="searchQuery"
-      label="ค้นหาสินค้า"
-      prepend-inner-icon="mdi-magnify"
-      clearable
-      @click:clear="searchQuery = ''"
-    ></v-text-field>
+        <v-text-field
+          v-model="searchQuery"
+          label="ค้นหาสินค้า"
+          prepend-inner-icon="mdi-magnify"
+          clearable
+          @click:clear="searchQuery = ''"
+        ></v-text-field>
       </v-col>
       <v-col v-for="product in filteredProducts" :key="product.id" cols="12" sm="6" md="4" lg="3">
-        <v-card class="hover-card" style="height: 400px" @click="goToDetail(product)">
+        <v-card class="hover-card" style="height: 400px">
           <v-img :src="product.image" style="height: 260px"></v-img>
 
           <v-card-title>{{ product.title }}</v-card-title>
 
           <v-card-subtitle>ราคา: {{ product.price }} บาท</v-card-subtitle>
           <v-card-actions>
-            <v-btn color="primary"> ดูรายละเอียด </v-btn>
+            <v-btn color="primary" variant="outlined" size="small" @click="goToDetail(product)">
+              ดูรายละเอียด
+            </v-btn>
+            <v-btn
+              class="ml-auto"
+              color="orange"
+              size="small"
+              variant="tonal"
+              @click="addToCart(product)"
+            >
+              <v-icon>mdi-cart</v-icon>
+              เพิ่มลงในตะกร้า
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -30,19 +42,19 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
 import { useRouter } from "vue-router";
+import { cart } from "@/plugins/stores/cart";
 
+const cartStore = cart();
 const props = defineProps<{ products: any[] }>();
 const router = useRouter();
 
-const searchQuery = ref('')
-
+const searchQuery = ref("");
 
 const filteredProducts = computed(() =>
   props.products.filter((product) =>
     product.title.toLowerCase().includes(searchQuery.value.toLowerCase())
   )
-
-)
+);
 
 function goToDetail(product: any) {
   router.push({
@@ -50,12 +62,15 @@ function goToDetail(product: any) {
     params: { id: product.id },
   });
 }
+
+function addToCart(product: any) {
+  cartStore.addToCart(product);
+}
 </script>
 
 <style scoped>
 .hover-card {
   transition: transform 0.3s ease, box-shadow 0.3s ease;
-  cursor: pointer;
 }
 .hover-card:hover {
   transform: scale(1.05);
